f5bbd0bb7edea9a5783dd04be9413e85f451f860
Quinton Wall <qwall@salesforce.com>
2014-10-27 13:59:08 -0700

    added oauth2 reps

--- Patagonia.playground/section-1.swift
+++ Patagonia.playground/section-1.swift
@@ -4,6 +4,12 @@ import UIKit
 import XCPlayground
 import Foundation
 import Alamofire
+import OAuth2
+
+
+
+
+//MAKE SURE ALAMOFIRE WORKS
 
 // Allow network requests to complete
 XCPSetExecutionShouldContinueIndefinitely()
@@ -16,6 +22,31 @@ Alamofire.request(.GET, "http://quinton.me")
         println(response)
         println(error)
     }
+
+//MAKE SURE OAUTH2 LIB WORKS
+let settings = [
+    "client_id" : "my_swift_app",
+    "client_secret" : "C7447242-A0CF-47C5-BAC7-B38BA91970A9",
+    "authorize_u:i" : "https://authorize.smartplatforms.org/authorize",
+    "token_uri" : "https://authorize.smartplatforms.org/token"
+]
+
+
+let oauth = OAuth2CodeGrant(settings: settings)
+
+
+oauth.onAuthorize = { parameters in
+    println("Did authorize with parameters: \(parameters)")
+}
+oauth.onFailure = { error in        // `error` is nil on cancel
+    if nil != error {
+        println("Authorization went wrong: \(error!.localizedDescription)")
+    }
+}
+
+
+
+
 
 
 
---
+++ patagonia/Patgonia.swift
@@ -0,0 +1,14 @@
+//
+//  Patgonia.swift
+//  Ultralight library for connecting to Salesforce.
+// Just pack what you need!
+//
+//  Created by Quinton Wall on 10/27/14.
+//  Copyright (c) 2014 Quinton Wall. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+
